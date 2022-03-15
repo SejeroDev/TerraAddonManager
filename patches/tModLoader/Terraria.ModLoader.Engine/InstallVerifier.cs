13b701b4b7c0959c98a9defefad18bedae91e408
TheStachelfisch <TheStachelfisch@users.noreply.github.com>
2022-03-15 22:43:58 +0100

    Update to 0.11.8.8

--- patches/tModLoader/Terraria.ModLoader.Engine/InstallVerifier.cs
+++ patches/tModLoader/Terraria.ModLoader.Engine/InstallVerifier.cs
@@ -30,20 +30,20 @@ namespace Terraria.ModLoader.Engine
 			if (Platform.IsWindows) {
 				steamAPIPath = "steam_api.dll";
 				steamAPIHash = ToByteArray("7B857C897BC69313E4936DC3DCCE5193");
-				gogHash = ToByteArray("6352ded8d64d0f67fdf10ff2a6f9e51f"); // Don't forget to update CheckExe in CheckGoG
-				steamHash = ToByteArray("201707bba92e27f09d05529e2f051c60");
+				gogHash = ToByteArray("0d4005c39a12a334d9bfd42dd5b656cc"); // Don't forget to update CheckExe in CheckGoG
+				steamHash = ToByteArray("5f321196521790a18a19d44fd066044e");
 			}
 			else if (Platform.IsOSX) {
 				steamAPIPath = "osx/libsteam_api.dylib";
 				steamAPIHash = ToByteArray("4EECD26A0CDF89F90D4FF26ECAD37BE0");
-				gogHash = ToByteArray("7b8d96e0ef583164d565dc01be4b5627");
-				steamHash = ToByteArray("0b253fbe529ea3e2ac61a0658f43af94");
+				gogHash = ToByteArray("f483f6f795e5c045b73c330015e852a6");
+				steamHash = ToByteArray("c3b967ddc50f400dc1575de05979ee47");
 			}
 			else if (Platform.IsLinux) {
 				steamAPIPath = "lib/libsteam_api.so";
 				steamAPIHash = ToByteArray("7B74FD4C207D22DB91B4B649A44467F6");
-				gogHash = ToByteArray("fa53f0a39be5698da7a15a1cc9e56689");
-				steamHash = ToByteArray("ab57cfd9076ab0c0eab9f46a412b8422");
+				gogHash = ToByteArray("56794421993db33b7607d1be233b586d");
+				steamHash = ToByteArray("b08ed3b4fe5417e7cd56e06ad99f2ab7");
 			}
 			else {
 				string message = Language.GetTextValue("tModLoader.UnknownVerificationOS");
@@ -122,7 +122,7 @@ namespace Terraria.ModLoader.Engine
 			IsGoG = true;
 
 			const string DefaultExe = "Terraria.exe";
-			string CheckExe = $"Terraria_1.4.3.2.exe"; // This should match the hashes. {Main.versionNumber}
+			string CheckExe = $"Terraria_1.4.3.6_B.exe"; // This should match the hashes. {Main.versionNumber}
 			string vanillaPath = File.Exists(CheckExe) ? CheckExe : DefaultExe;
 
 			// If .exe not present, check Terraria directory (Side-by-Side Manual Install)
